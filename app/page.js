af8e11a6c9406955e7ce75ab4802b8b7fd6367c7
Alex <inema9886@gmail.com>
2025-08-23 13:42:06 +0100

    changes made

--- app/page.js
+++ app/page.js
@@ -1,22 +1,12 @@
 import React from 'react'
 
 import Navbar from '@/components/Navbar'
-import HeroSection from '@/components/HeroSection'
-import AboutSection from '@/components/AboutSection'
-import ProjectsSection from '@/components/Projects'
-import ClientListSection from '@/components/ClientListSection'
-import ExperiencesSection from '@/components/Experience'
 
 function page() {
   return (
     <div>
      <Navbar/>
-      <HeroSection/>
-      <AboutSection/>
-      <ProjectsSection/>
-      <ClientListSection/>
-      <ExperiencesSection/>
-    
+     
     
     </div>
   )
